45c5f84235b1f0d35bcc27391980e2561bc5fa3e
ZatCq <kuanhux2007127@126.com>
2021-05-30 11:23:40 +0800

    登录

---
+++ app/src/main/java/com/example/admin/yuejian_22/Bean/SimpleResult.java
@@ -0,0 +1,23 @@
+package com.example.admin.yuejian_22.Bean;
+
+/**
+ * Created by lenovo on 2017/10/10.
+ */
+
+public class SimpleResult {
+
+    /**
+     * success : 0
+     */
+
+
+    private String success;
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+}
--- app/src/main/java/com/example/admin/yuejian_22/MainActivity.java
+++ app/src/main/java/com/example/admin/yuejian_22/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.admin.yuejian_22;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import com.example.admin.yuejian_22.Base.BaseActivity;
+import com.example.admin.yuejian_22.Fragment.LoginFragment;
 import com.example.admin.yuejian_22.Fragment.MainFragment;
 
 public class MainActivity extends BaseActivity {
     private FragmentManager fragmentManager;
     private FragmentTransaction transaction;
+    private LoginFragment loginFragment;
     private MainFragment mainFragment;
     private SharedPreferences preferences;
-    private final String fileName = "yuejain";
+    private final String fileName = "yuejian";
     private final int mode = MODE_PRIVATE;
-
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -31,7 +31,18 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         preferences = getSharedPreferences(fileName,mode);
         fragmentManager = getSupportFragmentManager();
-        showMainFragment();
+        boolean loginState = getLoginState();
+        if(loginState){
+            //直接进入主界面
+            setUserId(getSavedUserId());
+            showMainFragment();
+
+
+        }else{
+            //显示登录
+            showLoginFragment();
+
+        }
     }
     private  void showMainFragment(){
         transaction = fragmentManager.beginTransaction();
@@ -41,5 +52,37 @@ public class MainActivity extends BaseActivity {
 
         transaction.commit();
 
+    }
+
+    private  void showLoginFragment(){
+        transaction = fragmentManager.beginTransaction();
+
+        loginFragment = new LoginFragment();
+        transaction.add(R.id.fragment_holder,loginFragment);
+
+        transaction.commit();
+
+    }
+    public  void replaceLoginFragment(){
+        mainFragment = new MainFragment();
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_holder,mainFragment);
+        transaction.commit();
+    }
+    private boolean getLoginState(){
+
+        userId=getSavedUserId();
+
+        return !userId.equals("-1");
+
+    }
+    private String getSavedUserId(){
+        String userId = preferences.getString("user_id","-1");
+        return userId;
+    }
+    public void setSavedUserId(String userId){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("user_id",userId);
+        editor.commit();
     }
 }
