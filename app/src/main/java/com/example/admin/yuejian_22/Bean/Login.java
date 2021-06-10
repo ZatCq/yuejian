f966275df504ec68ea77ce30b7890c9b37fcb74e
ZatCq <kuanhux2007127@126.com>
2021-06-10 15:22:07 +0800

    update

---
+++ app/src/main/java/com/example/admin/yuejian_22/Bean/Login.java
@@ -0,0 +1,21 @@
+package com.example.admin.yuejian_22.Bean;
+
+/**
+ * Created by lenovo on 2017/10/10.
+ */
+
+public class Login {
+    /**
+     * userid : 1
+     */
+
+    private String userid;
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+}
